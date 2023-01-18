c048532a46586319be8707c356353f48c7fd5c13
yehia <3202114888@alexu.edu.eg>
2023-01-18 23:06:56 +0200

    fix issues

--- src/App.vue
+++ src/App.vue
@@ -259,7 +259,7 @@ export default {
     };
   },
   mounted() {
-    localStorage.clear("task") &&
+    localStorage.getItem("task") &&
       (this.tasksValue = JSON.parse(localStorage.getItem("task")));
     localStorage.getItem("todo") &&
       (this.todo = JSON.parse(localStorage.getItem("todo")));
